86d2ee4482f73f69ee8f7630906420806989f807
Laurence Woodward <Laurence@Laurences-MBP.lan>
2015-12-24 23:03:22 +0000

    Initial Commit

---
+++ Domain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Castle.Windsor;
+using Microsoft.Practices.ServiceLocation;
+using Castle.Windsor.Installer;
+
+namespace ProjectEuler.Domain
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			ProblemSolver.SolveProblem(null);
+		}
+
+		public static void SetupWindsorContainer()
+		{
+			throw new NotImplementedException ("This does not work, sounds like missing mscorlib dll");
+			//			var container = new WindsorContainer();
+			//			container.Install(Configuration.FromAppConfig());
+		}
+	}
+}
+
